07e402b30ad50b44a8b8f6d762d46c451f8dc70e
MustfaOzcan <m.ustafa190529@gmail.com>
2025-04-01 16:30:17 +0300

    Similation(main) update.

--- SpaceTravel/src/packet/Simulation.java
+++ SpaceTravel/src/packet/Simulation.java
@@ -10,9 +10,11 @@ public class Simulation {
         // Dosyaları oku
         Map<String, Planet> planets = FileReader.readPlanets("src/resources/Gezegenler.txt");
         List<Spacecraft> spacecrafts = FileReader.readSpacecrafts("src/resources/Araclar.txt");
+        List<Person> people = FileReader.readPeople("src/resources/Kisiler.txt");
         
         int iteration = 0;
         boolean allArrived = false;
+        
         
         while (!allArrived) {
             iteration++;
@@ -25,8 +27,17 @@ public class Simulation {
                 planet.advanceTime(1); // Her iterasyonda 1 saat ilerle
             }
             
+            // Kişilerin durumunu güncelle
+            for (Person person : people) {
+                person.passTime(1); // Her kişinin zamanını ilerlet
+            }
+            
             // Uzay araçlarının durumunu güncelle
             for (Spacecraft spacecraft : spacecrafts) {
+            	
+            	// Mürettebatın durumunu kontrol et
+                spacecraft.checkCrewStatus(people);
+                
             	// Çıkış ve varış gezegenlerini al
                 Planet departurePlanet = planets.get(spacecraft.getDeparturePlanet());
                 Planet destinationPlanet = planets.get(spacecraft.getDestinationPlanet());
@@ -39,7 +50,7 @@ public class Simulation {
             }
             
             // Ekrana bilgileri yaz
-            displayInformation(iteration, planets, spacecrafts);
+            displayInformation(iteration, planets, spacecrafts, people);
             
             // Tüm araçlar hedeflerine ulaştı mı kontrol et
             allArrived = checkAllArrived(spacecrafts);
@@ -50,25 +61,24 @@ public class Simulation {
         
         // Program bitiminde son durumu göster
         clearScreen();
-        displayInformation(iteration, planets, spacecrafts);
+        displayInformation(iteration, planets, spacecrafts, people);
         System.out.println("\nSimülasyon tamamlandı. Tüm uzay araçları hedeflerine ulaştı.");
     }
     
-    private static void displayInformation(int iteration, Map<String, Planet> planets, List<Spacecraft> spacecrafts) {
+    private static void displayInformation(int iteration, Map<String, Planet> planets, List<Spacecraft> spacecrafts, List<Person> people ) {
         // İterasyon bilgisini göster
-    	System.out.println("-------------------------------------");
-        System.out.println("iterasyon (saat) : " + iteration);
-      
+    	System.out.println("-------------------------------------");//silinebilir
+        System.out.println("iterasyon (saat) : " + iteration);//silinebilir
         int planetFieldWidth = 16; // Gezegen sütunu genişliği 
-        int dateFieldWidth   = 15;
-        
+        int dateFieldWidth = 15;
+      
         // Gezegenleri yazdır
         System.out.print("Gezegenler :\t");
         for (Planet planet : planets.values()) {
             // Gezegen ismini 3 karakterlik bir alan içinde ortalayıp --- ekliyoruz.
             String planetName = planet.toString();
             String formattedPlanet = "" + center(planetName, 3) + "";
-            System.out.printf("%-" + planetFieldWidth + "s", formattedPlanet);
+            System.out.printf("%-" + planetFieldWidth  + "s", formattedPlanet);
         }
         System.out.println();
         
@@ -76,14 +86,21 @@ public class Simulation {
         System.out.print("Tarih: \t\t");
         for (Planet planet : planets.values()) {
             String date = planet.getCurrentDateAsString();
-            System.out.printf("%-" + dateFieldWidth + "s", date);
+            System.out.printf("%-" + dateFieldWidth  + "s", date);
+        }
+        System.out.println();
+        // Nüfus bilgisi
+        System.out.print("Nüfus: \t\t");
+        for (Planet planet : planets.values()) {
+            int population = calculatePlanetPopulation(planet.getName(), spacecrafts, people);
+            System.out.printf("%-" + dateFieldWidth  + "d", population);
         }
         
         System.out.println("\n\n");
         
         // Uzay araçları tablosunu yazdır
         System.out.println("Uzay Araçlari:");
-        System.out.println("Araç Adı\tDurum\t\tÇıkış\t\tVarış\t\tHedefe Kalan Saat\tHedefe Varacağı Tarih");
+        System.out.println("Araç Adı\tDurum\t\tÇıkış\t\tVarış\t\tHedefe Kalan Saat\tHedefe Varacağı Tarih\tCrew");
         for (Spacecraft spacecraft : spacecrafts) {
         	System.out.printf("%-15s\t%-10s\t%-15s\t%-15s\t%-25.0f\t%-50s%n",  
                    spacecraft.getName(),
@@ -91,15 +108,72 @@ public class Simulation {
                    spacecraft.getDeparturePlanet(),
                    spacecraft.getDestinationPlanet(),
                    spacecraft.getRemainingHours(),
-                   spacecraft.getEstimatedArrivalDate()
+                   spacecraft.getEstimatedArrivalDate(),
+                   countCrewMembers(spacecraft.getName(), people)
                   
                    
                   );
         }
-       
+        
+        System.out.println("\n\nYolcular:");
+        System.out.println("İsim\t\tYaş\tKalan Ömür (saat)\tUzay Aracı\tDurum");
+        for (Person person : people) {
+            System.out.printf("%-15s\t%-5d\t%-20d\t%-15s\t%-10s%n",
+                   person.getName(),
+                   person.getAge(),
+                   person.getRemainingLifespan(),
+                   person.getSpacecraftName(),
+                   person.isAlive() ? "Yaşıyor" : "Ölü"
+                  );
+        }
     }
     
-    private static boolean checkAllArrived(List<Spacecraft> spacecrafts) {
+    
+
+	private static int calculatePlanetPopulation(String planetName, List<Spacecraft> spacecrafts, List<Person> people) {
+		int population = 0;
+		for (Person person : people) {
+            if (!person.isAlive()) {
+                continue; // Ölü insanları sayma
+            }
+            
+            String spacecraftName = person.getSpacecraftName();
+            Spacecraft spacecraft = findSpacecraft(spacecraftName, spacecrafts);
+            
+            if (spacecraft != null && !spacecraft.isDestroyed()) {
+                String location = spacecraft.getCurrentLocation();
+                if (location.equals(planetName)) {
+                    population++;
+                }
+            }
+        }
+		
+		return population;
+		
+		
+	}
+	
+
+	private static int countCrewMembers(String spacecraftName, List<Person> people) {
+        int count = 0;
+        for (Person person : people) {
+            if (person.getSpacecraftName().equals(spacecraftName) && person.isAlive()) {
+                count++;
+            }
+        }
+        return count;
+    }
+	
+	private static Spacecraft findSpacecraft(String name, List<Spacecraft> spacecrafts) {
+        for (Spacecraft spacecraft : spacecrafts) {
+            if (spacecraft.getName().equals(name)) {
+                return spacecraft;
+            }
+        }
+        return null;
+    }
+
+	private static boolean checkAllArrived(List<Spacecraft> spacecrafts) {
         for (Spacecraft spacecraft : spacecrafts) {
             if (!spacecraft.hasArrived()) {
                 return false;
