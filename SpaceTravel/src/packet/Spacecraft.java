c4c160ffd8a61fb192e9bdebf3d9ab2ca79a8c48
MustfaOzcan <m.ustafa190529@gmail.com>
2025-04-01 15:53:22 +0300

    Spacecraft model updated.

--- SpaceTravel/src/packet/Spacecraft.java
+++ SpaceTravel/src/packet/Spacecraft.java
@@ -1,6 +1,7 @@
 package packet;
 
 import java.text.ParseException;
+import java.util.List;
 
 public class Spacecraft {
     private String name;
@@ -11,6 +12,7 @@ public class Spacecraft {
     private double remainingHours;
     private String status;
     private String estimatedArrivalDate;
+    private boolean isDestroyed;
     
     public Spacecraft(String name, String departurePlanet, String destinationPlanet, 
                       String departureDate, double distanceInHours) {
@@ -22,15 +24,23 @@ public class Spacecraft {
         this.remainingHours = distanceInHours;
         this.status = "Bekliyor";
         this.estimatedArrivalDate = "Hesaplanıyor";
+        this.isDestroyed=false;
     }
     
  
     
     public void updateStatus(Planet departurePlanet, Planet destinationPlanet) throws ParseException {
     	
-    	 if (estimatedArrivalDate.equals("Hesaplanıyor")){
+    	if(isDestroyed)	{
+    		
+    		status = "İMHA";
+            return;
+    	}
+    	
+    	if (estimatedArrivalDate.equals("Hesaplanıyor")){
+    		
     	        calculateInitialEstimatedArrivalDate(departurePlanet, destinationPlanet);
-    	    }
+    	}
     	
         // Eğer araç hala beklemede ise ve bulunduğu gezegendeki tarih, çıkış tarihine eşitse
         if (status.equals("Bekliyor") && this.departurePlanet.equals(departurePlanet.getName()) && departurePlanet.isSameDate(departureDate)) {
@@ -40,6 +50,9 @@ public class Spacecraft {
     }
     
     public void travel(int hours) {
+    	if (isDestroyed) {
+            return;
+        }
         if (status.equals("Yolda")) {
             remainingHours -= hours;
             if (remainingHours <= 0) {
@@ -49,43 +62,25 @@ public class Spacecraft {
         }
     }
     
-    public String getName() {
-        return name;
+    public void checkCrewStatus(List<Person> people) {
+        // Aracın içinde yaşayan biri var mı kontrol et
+        boolean anyAlive = false;
+        
+        for (Person person : people) {
+            if (person.getSpacecraftName().equals(this.name) && person.isAlive()) {
+                anyAlive = true;
+                break;
+            }
+        }
+        
+        // Eğer kimse hayatta değilse aracı imha et
+        if (!anyAlive) {
+            isDestroyed = true;
+            status = "İMHA";
+        }
     }
     
-    public String getDeparturePlanet() {
-        return departurePlanet;
-    }
-    
-    public String getDestinationPlanet() {
-        return destinationPlanet;
-    }
-    
-    public String getDepartureDate() {
-        return departureDate;
-    }
-    
-    public double getDistanceInHours() {
-        return distanceInHours;
-    }
-    
-    public double getRemainingHours() {
-        return remainingHours;
-    }
-    
-    public String getStatus() {
-        return status;
-    }
-    
-    public boolean hasArrived() {
-        return status.equals("Varış");
-    }
-    
-    public String getEstimatedArrivalDate() {
-        return estimatedArrivalDate;
-    }
-    
-    // Simülasyon başında tahmini varış tarihini hesaplamak için
+    //  varış tarihini hesaplamak için
     public void calculateInitialEstimatedArrivalDate(Planet departurePlanet, Planet destinationPlanet) throws ParseException {
         // TimeCalculation sınıfını kullanarak varış tarihini hesapla
         String arrivalDate = TimeCalculation.calculateCompleteJourneyDate(
@@ -99,6 +94,32 @@ public class Spacecraft {
         
         this.estimatedArrivalDate = arrivalDate;
     }
+    
+   
+    public String getCurrentLocation() {
+        if (status.equals("Bekliyor")) {
+            return departurePlanet;
+        } else if (status.equals("Vardı")) {
+            return destinationPlanet;
+        } else {
+            return "Yolda";
+        }
+    }   
+        
+    public String getName() {return name;}
+    public String getDeparturePlanet() { return departurePlanet;}
+    public String getDestinationPlanet() {return destinationPlanet;}
+    public String getDepartureDate() {return departureDate;}
+    public double getDistanceInHours() { return distanceInHours;}
+    public double getRemainingHours() {return remainingHours;}
+    public String getStatus() {return status;}
+    public boolean hasArrived() {return status.equals("Vardı") || isDestroyed;}
+    public String getEstimatedArrivalDate() {return isDestroyed ? "İMHA" : estimatedArrivalDate;} 
+    public boolean isDestroyed() {return isDestroyed;}
+    
+    
+    
+   
     
     
 }
